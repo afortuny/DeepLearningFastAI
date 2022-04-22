ee34afbca7a663939b853e1b35bf82c880ae2932
Alan <afortuny@users.noreply.github.com>
2022-04-22 08:52:52 +0200

    Update RunningShoeAPP.py

--- RunningShoeAPP.py
+++ RunningShoeAPP.py
@@ -1,19 +1,38 @@
-from IPython.display import Image
-Image(filename="/content/Brands.JPG")
-import ipywidgets as widgets
+from fastai.vision.widgets import *
 from fastai.vision.all import *
-from fastai.vision.widgets import * 
-learn_inf = load_learner('export.pkl')
-btn_upload = widgets.FileUpload()
-out_pl = widgets.Output()
-lbl_pred = widgets.Label()
-def on_data_change(change):
-    lbl_pred.value = ''
-    img = PILImage.create(btn_upload.data[-1])
-    out_pl.clear_output()
-    with out_pl: display(img.to_thumb(128,128))
-    pred,pred_idx,probs = learn_inf.predict(img)
-    lbl_pred.value = f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}'
-btn_upload.observe(on_data_change, names=['data'])
-display(VBox([widgets.Label('Upload your running shoe!'), btn_upload, out_pl, lbl_pred]))
 
+from pathlib import Path
+
+import streamlit as st
+
+class Predict:
+    def __init__(self, filename):
+        self.learn_inference = load_learner(Path()/filename)
+        self.img = self.get_image_from_upload()
+        if self.img is not None:
+            self.display_output()
+            self.get_prediction()
+    
+    @staticmethod
+    def get_image_from_upload():
+        uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
+        if uploaded_file is not None:
+            return PILImage.create((uploaded_file))
+        return None
+
+    def display_output(self):
+        st.image(self.img.to_thumb(128,128), caption='Uploaded Image')
+
+    def get_prediction(self):
+
+        if st.button('Classify'):
+            pred, pred_idx, probs = self.learn_inference.predict(self.img)
+            st.write(f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}')
+        else: 
+            st.write(f'Click the button to classify') 
+
+if __name__=='__main__':
+
+    file_name='export.pkl'
+
+    predictor = Predict(file_name)
